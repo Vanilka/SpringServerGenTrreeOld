11cb887d153d49b1db377bda5f85fce7de35a051
Vanilka <szymkowiak.martyna@gmail.com>
2017-10-16 14:30:34 +0200

    no message

---
+++ GenealogyTreeProject/gentree.client.visualization.elements.gustave/src/main/java/gentree/client/visualization/controls/AgeDessins.java
@@ -0,0 +1,22 @@
+package gentree.client.visualization.controls;
+
+import javafx.scene.shape.Path;
+
+import java.nio.file.Paths;
+
+/**
+ * Created by Martyna SZYMKOWIAK on 14/10/2017.
+ */
+public class AgeDessins {
+
+    private static final Path ADULT = drawAdult();
+
+
+    private static Path drawAdult() {
+        Path p = new Path();
+
+
+
+        return p;
+    }
+}
