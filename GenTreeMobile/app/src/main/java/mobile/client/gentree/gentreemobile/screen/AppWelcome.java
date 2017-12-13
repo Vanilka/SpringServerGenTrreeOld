8c5b6dd320afb409094f4cf8a8e137208c861e87
Vanilka <szymkowiak.martyna@gmail.com>
2017-12-13 01:33:10 +0100

    Add snackbar

--- GenTreeMobile/app/src/main/java/mobile/client/gentree/gentreemobile/screen/AppWelcome.java
+++ GenTreeMobile/app/src/main/java/mobile/client/gentree/gentreemobile/screen/AppWelcome.java
@@ -1,19 +1,20 @@
-package mobile.client.gentree.gentreemobile;
+package mobile.client.gentree.gentreemobile.screen;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import mobile.client.gentree.gentreemobile.R;
 import mobile.client.gentree.gentreemobile.configuration.utilities.LoginHelper;
 
 public class AppWelcome extends AppCompatActivity {
 
     private EditText loginField;
     private EditText passwordField;
-    private TextView ERROR_LABEL;
     private Button loginButton;
 
     @Override
@@ -27,7 +28,6 @@ public class AppWelcome extends AppCompatActivity {
         loginField = (EditText) findViewById(R.id.loginField);
         passwordField = (EditText) findViewById(R.id.passwordField);
         loginButton = (Button) findViewById(R.id.loginButton);
-        ERROR_LABEL = (TextView) findViewById(R.id.errorLabel);
     }
 
     public void connect(View view) {
@@ -35,10 +35,12 @@ public class AppWelcome extends AppCompatActivity {
         String password = passwordField.getText().toString();
 
         if (LoginHelper.isNotEmpty(login) && LoginHelper.isNotEmpty(password)) {
-            ERROR_LABEL.setText("");
+            Snackbar.make(view, "Login OK", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
             
         } else {
-            ERROR_LABEL.setText("ERROR OCCURED");
+            Snackbar.make(view, "ERROR OCCURED", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
 
         }
     }
