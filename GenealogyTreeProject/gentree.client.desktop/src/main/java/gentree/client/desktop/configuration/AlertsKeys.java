0ac08590dac96d993729eae7a4f8b39afc09b567
Vanilka <szymkowiak.martyna@gmail.com>
2018-01-10 23:23:12 +0100

    Mobile

--- GenealogyTreeProject/gentree.client.desktop/src/main/java/gentree/client/desktop/configuration/AlertsKeys.java
+++
@@ -1,9 +0,0 @@
-package gentree.client.desktop.configuration;
-
-/**
- * Created by Martyna SZYMKOWIAK on 17/11/2017.
- */
-public class AlertsKeys {
-
-    public static final String CONNECTION_ERROR_HEADER = "connection.error.header";
-}
--- GenealogyTreeProject/gentree.client.visualization.elements.gustave/src/main/java/gentree/client/visualization/elements/configuration/AutoCleanable.java
+++
@@ -1,8 +0,0 @@
-package gentree.client.visualization.elements.configuration;
-
-/**
- * Created by Martyna SZYMKOWIAK on 17/11/2017.
- */
-public interface AutoCleanable {
-    void clean();
-}
